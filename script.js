36f289dd7e16e82dab4b528afe81e003ce1d4b66
Cade <cslayden3@gmail.com>
2023-02-07 17:27:59 -0700

    make changes to api, and update variables

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
 let findInfoButton = document.getElementById("findInfo")
 
-let country = document.getElementById("country")
-let zipcode = document.getElementById("zipcode")
+let county = document.getElementById("county")
 
 findInfoButton.addEventListener('click', logData)
 
-const apiUrl = 'https://api.zippopotam.us'
+const apiUrl = 'https://nominatim.openstreetmap.org/search?format=json&county='
 
 function logData(e){
     e.preventDefault(); 
     
-    fetch(`${apiUrl}/${country.value}/${zipcode.value}`, {
+    fetch(`${apiUrl}${county.value}`, {
         method: 'GET'
     })
     .then ((resp => resp.json()))
-    .then(data => renderData(data))
-    .catch((error) => {
-        console.error('Unvalid Zipcode', error)
-    })
+    .then(data => console.log(data))
 }
 
-function renderData(data){
-    const dataToArray = Object.values(data);
-    const useableData = Object.values(dataToArray[3]);
-    const displayValues = Object.values(useableData[0])
+// function renderData(data){
+//     const dataToArray = Object.values(data);
+//     const useableData = Object.values(dataToArray[3]);
+//     const displayValues = Object.values(useableData[0])
 
-    let townName = displayValues[0];
-    let longitude = displayValues[1];
-    let state = displayValues[2];
-    let stateAb = displayValues[3];
-    let latitude = displayValues[4];
+//     let townName = displayValues[0];
+//     let longitude = displayValues[1];
+//     let state = displayValues[2];
+//     let stateAb = displayValues[3];
+//     let latitude = displayValues[4];
 
-    let infoTown = document.createElement('p')
-    let infoLong = document.createElement('p')
-    let infoState = document.createElement('p')
-    let infoStateAb = document.createElement('p')
-    let infoLat = document.createElement('p')
+//     let infoTown = document.createElement('p')
+//     let infoLong = document.createElement('p')
+//     let infoState = document.createElement('p')
+//     let infoStateAb = document.createElement('p')
+//     let infoLat = document.createElement('p')
 
-    infoTown.classList.add('town')
-    infoLong.classList.add('longitude')
-    infoState.classList.add('state')
-    infoStateAb.classList.add('abbreviation')
-    infoLat.classList.add('latitude')
+//     infoTown.classList.add('town')
+//     infoLong.classList.add('longitude')
+//     infoState.classList.add('state')
+//     infoStateAb.classList.add('abbreviation')
+//     infoLat.classList.add('latitude')
 
-    infoTown.innerText = `City: ${townName}`
-    infoState.innerText = `State: ${state}`
-    infoLat.innerText = `Latitude: ${latitude}`
-    infoLong.innerText = `Longitude: ${longitude}`
-    infoStateAb.innerText = `State Abbreviation: ${stateAb}`
+//     infoTown.innerText = `City: ${townName}`
+//     infoState.innerText = `State: ${state}`
+//     infoLat.innerText = `Latitude: ${latitude}`
+//     infoLong.innerText = `Longitude: ${longitude}`
+//     infoStateAb.innerText = `State Abbreviation: ${stateAb}`
 
-    document.body.appendChild(infoTown)
-    document.body.appendChild(infoState)
-    document.body.appendChild(infoLat)
-    document.body.appendChild(infoLong)
-    document.body.appendChild(infoStateAb)
+//     document.body.appendChild(infoTown)
+//     document.body.appendChild(infoState)
+//     document.body.appendChild(infoLat)
+//     document.body.appendChild(infoLong)
+//     document.body.appendChild(infoStateAb)
 
-}
+// }
+
 let likeButton = document.getElementById("liker")
 let dislikeButton = document.getElementById("disliker")
 let dislikerList = document.getElementById("disliker-list")
@@ -67,13 +64,13 @@ dislikeButton.addEventListener('click', addDislike)
 function addLike(){
 
     let likeLi = document.createElement("li")
-    likeLi.innerText = `${zipcode.value}`
+    likeLi.innerText = `${county.value}`
     likerList.appendChild(likeLi)
 }
 function addDislike(){
 
     let dislikeLi = document.createElement("li")
-    dislikeLi.innerText = `${zipcode.value}`
+    dislikeLi.innerText = `${county.value}`
     dislikerList.appendChild(dislikeLi)
 }
 
