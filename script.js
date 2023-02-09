b71eb93fca62865456e9811be257a121080542c7
Cade <cslayden3@gmail.com>
2023-02-08 22:19:35 -0700

    straighten code

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
-let findInfoButton = document.getElementById("findInfo")
+let findInfoButton = document.getElementById("findInfo");
 
-let county = document.getElementById("county")
+let county = document.getElementById("county");
 
-findInfoButton.addEventListener('click', logData)
+findInfoButton.addEventListener("click", logData);
 
-const apiUrl = 'https://nominatim.openstreetmap.org/search?format=json&county='
+const apiUrl = "https://nominatim.openstreetmap.org/search?format=json&county=";
 
-function logData(e){
-    e.preventDefault(); 
-    
-    fetch(`${apiUrl}${county.value}`, {
-        method: 'GET'
-    })
-    .then ((resp => resp.json()))
-    .then(data => renderData(data))
-}
+function logData(e) {
+  e.preventDefault();
 
-let whichCounty = document.getElementById("pick-your-county")
+  if (document.getElementById("county-list")) {
+    document.getElementById("county-list").remove();
+  }
 
-let i = 0
-
-function renderData(data){
-    
-    whichCounty.innerText = "SEARCHING...";
-    
-    data.forEach(element => {
-    
-    let dataArray = Object.values(element);
-    let latitude = dataArray[5]
-    let longitude = dataArray[6]
-    let coordinates = (`${latitude},${longitude}`)
-    let selectCounty = dataArray[7]
-    let info = (`${selectCounty}. Latitude: ${latitude}, Longitude: ${longitude}`)
-
-    let countyList = document.getElementById("county-list")
-    
-    setTimeout(function(){
-        let listInfo = document.createElement('p')
-        listInfo.setAttribute(`id`, `county-${i}`)
-        node = document.createTextNode(info)
-        listInfo.appendChild(node)
-        countyList.appendChild(listInfo)
-    }, 3000)
-
-
-    i++
-
+  fetch(`${apiUrl}${county.value}`, {
+    method: "GET",
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      if (data.length) {
+        renderData(data);
+      } else {
+        error();
+      }
     });
-    
 }
 
+function error() {
+  setTimeout(function () {
+    let errorId = document.getElementById("error");
+    let node = document.createTextNode("No counties found!");
+    errorId.appendChild(node);
+  }, 3000);
+}
 
+let i = 0;
 
+function renderData(data) {
+
+  let whichCounty = document.getElementById("pick-your-county");
+
+  let createDiv = document.createElement("div");
+  createDiv.setAttribute("id", "county-list");
+  document.body.appendChild(createDiv);
+
+  whichCounty.innerText = "SEARCHING...";
+
+  data.forEach((element) => {
+    let dataArray = Object.values(element);
+    let latitude = dataArray[5];
+    let longitude = dataArray[6];
+    let selectCounty = dataArray[7];
+    let info = `${selectCounty}. Latitude: ${latitude}, Longitude: ${longitude}`;
+
+    let countyList = document.getElementById("county-list");
+
+    setTimeout(function () {
+      let listInfo = document.createElement("p");
+      listInfo.setAttribute(`id`, `county-${i}`);
+      node = document.createTextNode(info);
+      listInfo.appendChild(node);
+      countyList.appendChild(listInfo);
+    }, 3000);
+
+    setTimeout(function () {
+      whichCounty.innerText = "";
+    }, 3500);
+
+    i++;
+  });
+}
